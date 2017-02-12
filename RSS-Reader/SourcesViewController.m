2c6864cf214bd9dcef3a78d3044faac60f744289
Maria Saveleva <mariiasaveleva@gmail.com>
2017-02-12 14:45:28 +0300

    Added text fields

--- RSS-Reader/SourcesViewController.m
+++ RSS-Reader/SourcesViewController.m
@@ -13,7 +13,10 @@ static NSString * const kSrouceCellId = @"SrouceCellId";
 @interface SourcesViewController () <UITableViewDelegate, UITableViewDataSource>
 
 @property (nonatomic, strong) IBOutlet UITableView *tableView;
-@property (nonatomic, strong) UIAlertController *alertController;
+
+@property (nonatomic, strong, nullable) UIAlertController *alertController;
+@property (nonatomic, strong, nullable) UITextField *sourceTitleTextField;
+@property (nonatomic, strong, nullable) UITextField *sourceURLTextField;
 
 @end
 
@@ -28,28 +31,51 @@ static NSString * const kSrouceCellId = @"SrouceCellId";
 #pragma mark - Actions
 
 - (IBAction)addRSSSource:(id)sender {
-    [self presentViewController:self.alertController animated:YES completion:^{
-        //TODO: update data
-    }];
+    [self presentViewController:self.alertController animated:YES completion:nil];
 }
 
 #pragma mark - Private methods
 
 - (UIAlertController *)alertController {
     if (_alertController == nil) {
+        //alert controller
         _alertController = [UIAlertController alertControllerWithTitle:@"Add RSS source"
                                                                   message:@""
                                                            preferredStyle:UIAlertControllerStyleAlert];
 
+        //textfields
+        __weak typeof(self) weakSelf = self;
+        [_alertController addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+            weakSelf.sourceTitleTextField = textField;
+            textField.placeholder = @"Source title";
+        }];
+
+        [_alertController addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+            weakSelf.sourceURLTextField = textField;
+            textField.placeholder = @"Enter URL";
+        }];
+
+        //actions
         UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel"
                                                                style:UIAlertActionStyleCancel
-                                                             handler:^(UIAlertAction * _Nonnull action) {
-            //do nothing
-        }];
+                                                             handler:^(UIAlertAction * _Nonnull action) {}];
         [_alertController addAction:cancelAction];
 
-        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-            //TODO: implement
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                           style:UIAlertActionStyleDefault
+                                                         handler:^(UIAlertAction * _Nonnull action) {
+            BOOL isValidTitle = [weakSelf isValidTitle:weakSelf.sourceTitleTextField.text];
+            BOOL isValidURL = [weakSelf isValidLink:weakSelf.sourceURLTextField.text];
+            if (isValidTitle && isValidURL) {
+                //TODO: update data
+            } else {
+                if (!isValidTitle) {
+                    NSLog(@"Error: invalid title");
+                }
+                if (!isValidURL) {
+                    NSLog(@"Error: invalid URL");
+                }
+            }
         }];
         [_alertController addAction:okAction];
     }
